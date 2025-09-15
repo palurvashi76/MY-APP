7cdd0afb5e915d373cfcf811e78bf2903bbc6149
Urvashi Pal <palurvashi76@gmail.com>
2025-09-15 22:56:05 +0530

    Add Netlify redirect rule to fix page not found error

--- src/App.js
+++ src/App.js
@@ -1,12 +1,40 @@
-import React from 'react';
+import React, { useState } from 'react';
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
 import './App.css';
-import Counter from './Counter';
+import Banner from './Banner';
+import ShoppingCart from './ShoppingCart';
+import ProductPanel from './ProductPanel';
+import ProductDetails from './ProductDetails';
+import Chat from './Chat';
+import { CartContext } from './CartContext.js';
 
 function App() {
+  const [cartTotal, setCartTotal] = useState(0);
+  const contextValue = { cartTotal, setCartTotal };
+
   return (
-    <div className="App">
-      <Counter />
-    </div>
+    <BrowserRouter>
+      <CartContext.Provider value={contextValue}>
+        <div className="App">
+          <Banner />
+          <Routes>
+            {/* Route for the Home Page */}
+            <Route
+              path="/"
+              element={
+                <>
+                  <ShoppingCart />
+                  <ProductPanel />
+                  <Chat />
+                </>
+              }
+            />
+            {/* Route for the Product Details Page */}
+            <Route path="/product/:prodid" element={<ProductDetails />} />
+          </Routes>
+        </div>
+      </CartContext.Provider>
+    </BrowserRouter>
   );
 }
 
